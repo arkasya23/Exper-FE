4f019f150eb827df3df119b0cd762bcf90611199
Andreea Cirstoiu <andreea.cirstoiu23@yahoo.com>
2021-03-29 23:44:27 +0300

    added form validators and register api call

--- exper/src/app/app.module.ts
+++ exper/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,6 +6,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ApiProdService } from './shared/services/api.prod.service';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { RegisterComponent } from './components/register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- exper/src/app/components/register/register.component.ts
+++ exper/src/app/components/register/register.component.ts
@@ -1,5 +1,9 @@
+
+import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ApiProdService } from "src/app/shared/services/api.prod.service";
+import { RegisterModel } from "src/app/shared/models/register.model";
 
 @Component({
   selector: 'app-register',
@@ -9,14 +13,44 @@ import { FormControl, FormGroup } from "@angular/forms";
 export class RegisterComponent implements OnInit {
   signupForm: FormGroup;
 
+  constructor(private http: HttpClient, private apiService: ApiProdService){
+
+  }
+
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      'email': new FormControl(null),
-      'password': new FormControl(null),
-      'confirmPassword': new FormControl(null),
+      'email': new FormControl(null, Validators.email),
+      'password': new FormControl(null,[Validators.required, this.invalidPassword]),
+      'confirmPassword': new FormControl(null, Validators.required),
 
+    }, { validators: this.checkPasswords });
+  }
+
+  register() {
+    const registerPayload = new RegisterModel(this.signupForm.value.email, this.signupForm.value.password);
+    this.apiService.register(registerPayload).subscribe(token => {
+      this.apiService.updateToken(token);
+    },
+    err => {
+      console.log(err);
     });
   }
 
+  invalidPassword(control: FormControl): {[s: string]: boolean} {
+    const validPasswordExpr = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+    const valid = validPasswordExpr.test(control.value);
+  
+    if(valid === false) {
+      return {'invalidPassword': true}
+    }
 
+    return null;
+  }
+
+  checkPasswords(group: FormGroup) { 
+    const password = group.get('password').value;
+    const confirmPassword = group.get('confirmPassword').value;
+
+    return password === confirmPassword ? null : { notSame: true }     
+  }
 }
