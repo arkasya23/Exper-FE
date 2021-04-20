b7758eba279bb225557fd4d0e2a7f614534969da
Wazt <waztfm@gmail.com>
2021-04-20 21:46:21 +0300

    Hide the delete member button if the member is the current user or the trip owner

--- exper/src/app/components/dashboard/members/members.component.ts
+++ exper/src/app/components/dashboard/members/members.component.ts
@@ -1,21 +1,39 @@
-import { Component, EventEmitter, Input, Output} from "@angular/core";
+import { PersonalExpensesComponent } from './../personal-expenses/personal-expenses.component';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { DialogAddMembers } from "src/app/dialogs/add-members/add-members.component";
 import { TripService } from "src/app/shared/services/trip.service";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Component({
-    selector: 'app-members',
-    templateUrl: 'members.component.html',
-    styleUrls: ['members.component.scss']
+  selector: 'app-members',
+  templateUrl: 'members.component.html',
+  styleUrls: ['members.component.scss']
 })
-export class MembersComponent {
+export class MembersComponent implements OnInit {
+  
   @Input() trip;
   @Output() deletedUser = new EventEmitter<any>();
-  
+  private jwt = new JwtHelperService();
+
   constructor(
     public dialog: MatDialog,
     public tripService: TripService
-    ) {}
+  ) { }
+
+  ngOnInit(): void {
+    const currentUser = this.jwt.decodeToken(JSON.parse(sessionStorage.getItem('token')).token);
+    this.trip.users = this.trip.users.map(user => {
+      user.canDelete = true;
+      if (user.email === this.trip.createdByUser.email) {
+        user.canDelete = false;
+        user.email += ' (trip owner)';
+      } else if (user.email === currentUser.email) {
+        user.canDelete = false;
+      }
+      return user;
+    }).sort((a, b) => a.canDelete - b.canDelete);
+  }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogAddMembers, {
@@ -28,7 +46,7 @@ export class MembersComponent {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      if(result) {
+      if (result) {
         this.trip.users = result.users
       }
     });
